4702d1cddf64e773d63e446d074525d08a509099
MrBG0524 <105250807+MrBG0524@users.noreply.github.com>
2022-11-19 15:26:08 -0600

    Add files via upload

---
+++ cookiers6a/aq22honb18c/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean' -- if that doesn't work, try 'adamant' or 'bodacious'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq22honb18c_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq22honb18c_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq22honb18c'
